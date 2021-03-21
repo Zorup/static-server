4970a995394b7353cf37bb13f8cc4362edcab565
맥북 <macbook@maegbug-ui-MacBookPro.local>
2021-03-22 00:19:28 +0900

    initial commit

---
+++ src/main/java/com/zorup/statics/StaticApplication.java
@@ -0,0 +1,13 @@
+package com.zorup.statics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StaticApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/zorup/statics/controller/WebController.java
@@ -0,0 +1,18 @@
+package com.zorup.statics.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebController {
+
+    @GetMapping("/template")
+    public String viewPost(){
+        return "temp";
+    }
+
+    @GetMapping("/error")
+    public String error(){
+        return "error";
+    }
+}
