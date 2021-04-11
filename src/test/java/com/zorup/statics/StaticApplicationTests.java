82256da507fe26dd601aa57688e1664dd7baaca5
lophunju <lophunju@naver.com>
2021-04-11 18:38:09 +0900

    안쓰는 테스트 삭제

--- src/test/java/com/zorup/statics/StaticApplicationTests.java
+++ src/test/java/com/zorup/statics/StaticApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class StaticApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
