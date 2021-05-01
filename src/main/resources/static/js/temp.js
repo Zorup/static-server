1a5fb16cf729374c890df660f5b78a7bd56ece80
lophunju <lophunju@naver.com>
2021-05-02 02:13:43 +0900

    로그아웃 기능 완료

--- src/main/resources/static/js/temp.js
+++ src/main/resources/static/js/temp.js
@@ -139,6 +139,25 @@ initAjax();
 /**
  * 버튼동작들
  */
+function addForum(){
+    let forumName = $('#InputForumName').val();
+
+    $.ajax({
+        url: `${window.API_GATEWAY_URI}/main/v1/forum`,
+        type: 'POST',
+        data: {
+            forumName: forumName,
+        },
+        xhrFields: {
+            withCredentials: true
+        }
+    }).done(function(){
+        location.href = "/template";
+    }).fail(function(){
+        alert("유효하지 않은 입력이거나, 네트워크 오류가 존재합니다. 다시 시도해주세요.");
+    });
+}
+
 function deleteForum(){
     alert("delte");
 
@@ -161,36 +180,19 @@ function deleteForum(){
 
 function logout(){
     $.ajax({
-        url: '/v1/logout',
+        url: `${window.API_GATEWAY_URI}/main/v1/logout`,
         type: 'POST',
         xhrFields: {
             withCredentials: true
         }
     }).done(function(){
-        location.href="/login";
+        location.href=`/`;
     }).fail(function(){
         alert("실패하였습니다.");
     });
 }
 
-function addForum(){
-    let forumName = $('#InputForumName').val();
 
-    $.ajax({
-        url: `${window.API_GATEWAY_URI}/main/v1/forum`,
-        type: 'POST',
-        data: {
-            forumName: forumName,
-        },
-        xhrFields: {
-            withCredentials: true
-        }
-    }).done(function(){
-        location.href = "/template";
-    }).fail(function(){
-        alert("유효하지 않은 입력이거나, 네트워크 오류가 존재합니다. 다시 시도해주세요.");
-    });
-}
 
 function switchFeed(param){
     let url = '/template/' + param;
