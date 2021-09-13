52c5983ac5d9e23785d6b4e3d61105e7adcdbc16
김한성 <khs960616>
2021-09-13 14:59:44 +0900

    todolist

--- src/main/resources/static/js/firebase-app-init.js
+++ src/main/resources/static/js/firebase-app-init.js
@@ -26,3 +26,10 @@ messaging.requestPermission()
             console.log(payload.data);
         })
     })
+/*
+* TODO
+* 1. 개별 사용자 엔티티에 push_token을 저장하는 API 만든 후 push 토큰 저장.
+* 2. 각 message를 받은 이후 알람이 뜨는 영역에 list el을 동적으로 추가한다.
+* 3. 알람 뜨는 영역 클릭시 현재까지 있던 사용자의 알람은 모두 Read상태로 바꾸며 다음 로그인 시 더 이상 띄우지 않는다.
+* 4. 멘션 혹은 채팅 관련 모듈은 FCM 서버에게 보내, 멘션 혹은 채팅을 받는 사용자의 화면에 동적인 알람을 띄운다.
+* */
